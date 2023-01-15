e4b1fbbdcd8ce554b734c07459bae499043d0654
collins <chigbo.c.o@gmail.com>
2023-01-15 13:55:22 +0100

    made some adjustments

--- src/routes/NotAuthorized.tsx
+++ src/routes/NotAuthorized.tsx
@@ -1,17 +1,17 @@
-import {Paper, Box, Typography, Button} from '@mui/material';
+import {Paper, Typography, Button, Container} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 function NotAuthorized() {
 
   const navigate = useNavigate();
   return (
-    <Box sx={{m:20,display:"flexbox", alignItems:"center"}}>
+    <Container maxWidth="sm">
       <Button variant='contained' sx={{m:4}} onClick={() => navigate("/")}>Back to Home</Button>
       <Paper>
         <Typography variant='h5' component='p' sx={{p:4}}>Sorry, you are not authorized to access this page, <br/>
          as a Patient</Typography>
       </Paper>
-    </Box>
+    </Container>
   )
 }
 
